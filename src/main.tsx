0e4793cc02ef47171160b80f4ddf9f8de4f0298e
Vinicius Rocha <mvinicius.gbo@gmail.com>
2021-12-13 13:53:05 -0300

    Reset input styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 
 import App from './App';
 import { store } from "./global/store";
+import './global/form-reset.css';
 import './global/tailwind.css';
 import './index.css';
 
